d44d7b140cbb5993305d84bf225af0b34994aa62
Andrew Vuong <andrew.vuong@berkeley.edu>
2020-09-19 09:48:00 -0700

    Heroku config

--- helpers/github.js
+++ helpers/github.js
@@ -1,5 +1,5 @@
 const axios = require('axios');
-const config = require('../config.js');
+//const config = require('../config.js'); config.TOKEN
 
 let getReposByUsername = (username) => {
   // TODO - Use the axios module to request repos for a specific
@@ -11,7 +11,7 @@ let getReposByUsername = (username) => {
     url: `https://api.github.com/users/${username}/repos`,
     headers: {
       'User-Agent': 'request',
-      'Authorization': `token ${process.env.GITHUBTOKEN || config.TOKEN}`
+      'Authorization': `token ${process.env.GITHUBTOKEN}`
     }
   };
   // return a Promise object, resolves with data from github API if found
